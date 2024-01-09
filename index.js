a0d1dad9a91be66b7e49a95d0319a3f28c715ddf
Hatem <hatem.deux@gmail.com>
2024-01-09 12:12:56 -0500

    code: render graph header, bars, and labels from jQuery

--- index.js
+++ index.js
@@ -33,6 +33,29 @@ const illustrateBarLabels = function (data) {
 }
 
 const drawBarChart = function (data, options, element){
+  $(document).ready(function(){
+
+    $(".dashboard__graph")
+      .empty()
+      .append("<header></header>")
+      .append("<div></div>")
+      .children("header").addClass("graph__header").parent()
+      .children("div").addClass("graph__illustrator")
+
+    $(".graph__header")
+      .append("<h2></h2>").children("h2").text("Graph Title")
+      .parent()
+      .append("<p></p>").children("p").text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc et mi nec massa hendrerit efficitur. ")
+
+    $(".graph__illustrator")
+      .append("<div></div>").children("div:last-child").addClass("bar-labels-container")
+      .parent()
+      .append("<div></div>").children("div:last-child").addClass("vl")
+      .parent()
+      .append("<div></div>").children("div:last-child").addClass("bars-container")
+
+});
+
 
   data.forEach((element, index) => {
     console.log('element is -> ' + element + 'at index => ' + index);
@@ -42,7 +65,7 @@ const drawBarChart = function (data, options, element){
 
 };
 
-
+// test data
 const data = [25, 45, 87, 12, 100] // temp mock data
 const data01 = [
   { barLabel : "label-A1", barValue : 1 },
